814eccebe470859b2bb3ecd3a9e6e8a43ea22d47
dianasinenchenko <diana.sinenchenko@gmail.com>
2017-01-05 14:31:25 +0200

    Container done

--- src/main/java/school/lemon/changerequest/java/container/ContainerImpl.java
+++ src/main/java/school/lemon/changerequest/java/container/ContainerImpl.java
@@ -1,38 +1,67 @@
 package school.lemon.changerequest.java.container;
 
-public class ContainerImpl implements Container{
+import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
+
+import java.util.Arrays;
+
+public class ContainerImpl implements Container {
+    private int arr[];
+    private int size;
+
+    public ContainerImpl(int arr[]) {
+        this.arr = new int[Container.INITIAL_ARRAY_SIZE];
+        this.size = 0;
+
+    }
+
+    private void CheckSize() {
+        if (size == arr.length) {
+            int[] newArr = new int[arr.length * 10];
+            System.arraycopy(arr, 0, newArr, 0, arr.length);
+            arr = newArr;
+        }
+    }
+
 
     @Override
     public int size() {
-        // TODO: please implement me
-        return 0;
+        return size;// 
     }
 
     @Override
     public void clear() {
-        // TODO: please implement me
+        size = 0;
     }
 
     @Override
     public Integer get(int index) {
-        // TODO: please implement me
-        return null;
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        return arr[index];
     }
 
     @Override
     public void add(int element) {
-        // TODO: please implement me
+        CheckSize();
+        arr[size++] = element;
     }
 
     @Override
     public boolean add(int element, int index) {
-        // TODO: please implement me
-        return false;
+        if (index < 0 || index > size) return false;
+        CheckSize();
+        System.arraycopy(arr, index, arr, index + 1, size - index);
+        arr[index] = element;
+        ++size;
+        return true;
     }
 
     @Override
     public boolean remove(int index) {
-        // TODO: please implement me
-        return false;
+        if (index < 0 || index >= size) return false;
+        System.arraycopy(arr, index + 1, arr, index, size - index - 1);
+        --size;
+        return true;
     }
 }
